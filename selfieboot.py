b41a4a040ab505d21be1f0b3eb1d268dbc52bef1
Rein Appeldoorn <reinzor@gmail.com>
2015-04-03 19:02:59 +0000

    yes

--- selfieboot.py
+++ selfieboot.py
@@ -4,13 +4,33 @@ import picamera
 from PIL import Image
 from time import sleep
 
+screen_width = 1024
+screen_height = 768
+
 class Camera(picamera.PiCamera):
-    def add_img_overlay(self, filename, alpha = 255, layer = 99):
+    def add_img_overlay(self, filename, where=None, alpha = 255, layer = 99):
+        if not where or where != "top" or where != "bottom":
+            x = 0
+            y = 0
+
         img = Image.open(filename)
-        pad = Image.new('RGBA', ( ((img.size[0] + 31) // 32) * 32, ((img.size[1] + 15) // 16) * 16, ))
+
+        width = img.size[0]
+        height = img.size[1]
+
+        if where == "bottom":
+            x = 0
+            y = screen_height - height
+
+        pad = Image.new('RGB', ( ((width + 31) // 32) * 32, ((height + 15) // 16) * 16, ))
         pad.paste(img, (0, 0))
 
-        return camera.add_overlay(pad.tostring(), size=img.size, alpha=alpha, layer=layer)
+        return camera.add_overlay(pad.tostring(), 
+                size=img.size, 
+                fullscreen=False,
+                window=[x, y, width, height ], 
+                alpha=alpha, 
+                layer=layer)
 
 with Camera() as camera:
     camera.resolution = (1280, 720)
@@ -19,7 +39,7 @@ with Camera() as camera:
 
     # Wait indefinitely until the user terminates the script
     while True:
-        o = camera.add_img_overlay("assets/slide1.png")
+        o = camera.add_img_overlay("assets/overlay.png", where="bottom")
         sleep(1)
         camera.remove_overlay(o)
         sleep(1)
