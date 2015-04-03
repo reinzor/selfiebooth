2120b95e201d2aec2065a014ace86ee656e3473c
Rein Appeldoorn <reinzor@gmail.com>
2015-04-03 19:46:24 +0200

    update

--- selfieboot.py
+++ selfieboot.py
@@ -4,36 +4,23 @@ import picamera
 from PIL import Image
 from time import sleep
 
-with picamera.PiCamera() as camera:
+class Camera(picamera.PiCamera):
+    def add_img_overlay(self, filename, alpha = 255, layer = 99):
+        img = Image.open(filename)
+        pad = Image.new('RGB', ( ((img.size[0] + 31) // 32) * 32, ((img.size[1] + 15) // 16) * 16, ))
+        pad.paste(img, (0, 0))
+
+        return camera.add_overlay(pad.tostring(), size=img.size, alpha=alpha, layer=layer)
+
+with Camera() as camera:
     camera.resolution = (1280, 720)
     camera.framerate = 24
     camera.start_preview()
 
-    # Load the arbitrarily sized image
-    img = Image.open('assets/overlay.png')
-    # Create an image padded to the required size with
-    # mode 'RGB'
-
-    print img.size[0]
-    print img.size[1]
-
-    pad = Image.new('RGB', (
-        ((img.size[0] + 31) // 32) * 32,
-        ((img.size[1] + 15) // 16) * 16,
-        ))
-    # Paste the original image into the padded one
-    pad.paste(img, (0, 0))
-
-    # Add the overlay with the padded image as the source,
-    # but the original image's dimensions
-    o = camera.add_overlay(pad.tostring(), size=img.size)
-    # By default, the overlay is in layer 0, beneath the
-    # preview (which defaults to layer 2). Here we make
-    # the new overlay semi-transparent, then move it above
-    # the preview
-    o.alpha = 128
-    o.layer = 3
-
     # Wait indefinitely until the user terminates the script
     while True:
+        o = camera.add_img_overlay("assets/slide1.png")
         sleep(1)
+        camera.remove_overlay(o)
+        sleep(1)
+        
