244f40055f0ed93f9f42e2ab35b76e6f6247733b
Rein Appeldoorn <reinzor@gmail.com>
2015-04-04 12:11:41 +0000

    working boot

--- selfieboot.py
+++ selfieboot.py
@@ -2,7 +2,8 @@
 
 import os.path
 import sys
-if not os.path.isfile("/home/pi/selfieboot/images/alive"):
+
+if len(sys.argv) == 1 and not os.path.isfile("/home/pi/selfieboot/images/alive"):
     print "ERROR: NO USB STICK WITH ALIVE FILE FOUND IN 'images' FOLDER"
     sys.exit(1)
 
@@ -10,7 +11,7 @@ import picamera
 import io
 from PIL import Image
 from time import sleep, time
-from random import shuffle
+from collections import deque
 
 # GPIO
 import RPi.GPIO as GPIO
@@ -19,6 +20,7 @@ GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
 GPIO.add_event_detect(17, GPIO.FALLING)
 
 # PARAMS
+ROOT = "/home/pi/selfieboot/"
 COUNTDOWN_TIME = 4
 FREEZE_TIME = 4
 FLASH_TIME = 1
@@ -30,13 +32,10 @@ SCREENSAVER_SLIDE_TIME = 10
 screen_width_ = 1024
 screen_height_ = 768
 
-screensavers_ = []
-
 class Selfieboot(picamera.PiCamera):
     _time_last_press = 0
-    _screensavers = []
+    _screensavers = deque()
     _screensaver_overlay = None
-    _press_allowed = False
 
     def add_img_overlay(self, filename, where=None, fullscreen=False, alpha = 255, layer = 99):
         if not where or where != "top" or where != "bottom":
@@ -63,20 +62,21 @@ class Selfieboot(picamera.PiCamera):
                 layer=layer)
 
     def add_screensaver(self, path):
-        self._screensavers.append(path)
+        self._screensavers.appendleft(path)
 
     def check_screensaver(self):
         if time() - self._time_last_press > SCREENSAVER_TIME:
-            shuffle(self._screensavers)
+            self._screensavers.rotate()
             screensaver = self._screensavers[0]
 
             slide_start_time = time()
 
             if self._screensaver_overlay:
+                tmp_screensaver_overlay = self.add_img_overlay(screensaver, layer = 10)
                 self.remove_overlay(self._screensaver_overlay)
-                self._screensaver_overlay = None
-
-            self._screensaver_overlay = self.add_img_overlay(screensaver, layer = 10)
+                self._screensaver_overlay = tmp_screensaver_overlay
+            else:
+                self._screensaver_overlay = self.add_img_overlay(screensaver, layer = 10)
 
             while time() - slide_start_time < SCREENSAVER_SLIDE_TIME:
                 if self.push_button_pressed():
@@ -95,15 +95,15 @@ class Selfieboot(picamera.PiCamera):
             if countdown_overlay_time != time_left:
                 countdown_overlay_time = time_left
                 if countdown_overlay:
-                    tmp_countdown_overlay = self.add_img_overlay("assets/countdown%d.png"%time_left, layer = 11)
+                    tmp_countdown_overlay = self.add_img_overlay(ROOT + "assets/countdown%d.png"%time_left, layer = 11)
                     self.remove_overlay(countdown_overlay)
                     countdown_overlay = tmp_countdown_overlay
                 else:
-                    countdown_overlay = self.add_img_overlay("assets/countdown%d.png"%time_left, layer = 11)
+                    countdown_overlay = self.add_img_overlay(ROOT + "assets/countdown%d.png"%time_left, layer = 11)
 
-            sleep(0.01)
+            self.push_button_pressed(always_false=True)
 
-        flash_overlay = self.add_img_overlay("assets/white.png", layer = 14)
+        flash_overlay = self.add_img_overlay(ROOT + "assets/white.png", layer = 14)
 
         self.remove_overlay(countdown_overlay)
 
@@ -129,24 +129,22 @@ class Selfieboot(picamera.PiCamera):
                 alpha = int ( ( (FLASH_TIME - duration) / FLASH_TIME ) * 255 )
                 flash_overlay.alpha = alpha
 
-            sleep(0.01)
+            self.push_button_pressed(always_false=True)
 
         self.remove_overlay(flash_overlay)
         self.remove_overlay(capture_overlay)
 
         # Store the image
-        img.save("images/%d.jpeg"%int(press_time), "JPEG")
+        img.save(ROOT + "images/%d.jpeg"%int(press_time), "JPEG")
 
         self._time_last_picture = time()
 
-    def push_button_pressed(self):
+    def push_button_pressed(self, always_false=False):
         sleep(.01)
-        if GPIO.event_detected(17) and self._press_allowed:
+        if GPIO.event_detected(17) and time() - self._time_last_press > 1.0 and not always_false:
             self._time_last_press = time()
-            self._press_allowed = False
             return True
 
-        self._press_allowed = True
         return False
 
 with Selfieboot() as boot:
@@ -154,10 +152,13 @@ with Selfieboot() as boot:
     boot.framerate = 24
     boot.start_preview()
 
-    top_layer = boot.add_img_overlay("assets/top.png", where="top", layer=3)
-    bottom_layer = boot.add_img_overlay("assets/bottom.png", where="bottom", layer=4)
+    top_layer = boot.add_img_overlay(ROOT + "assets/top.png", where="top", layer=3)
+    bottom_layer = boot.add_img_overlay(ROOT + "assets/bottom.png", where="bottom", layer=4)
 
-    boot.add_screensaver("assets/screensaver.png")
+    boot.add_screensaver(ROOT + "assets/slide1.png")
+    boot.add_screensaver(ROOT + "assets/slide2.png")
+    boot.add_screensaver(ROOT + "assets/slide3.png")
+    boot.add_screensaver(ROOT + "assets/slide4.png")
 
     # Run it
     while True:
---
+++ test_gpio.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os.path
+import sys
+from time import sleep
+
+# GPIO
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+GPIO.add_event_detect(17, GPIO.FALLING)
+
+while True:
+    sleep(.01)
+    if GPIO.event_detected(17):
+        print "pressed" 
+
