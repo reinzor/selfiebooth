57e9b8de2262f50a2e1784a8c4606e58eaa055f0
Rein Appeldoorn <reinzor@gmail.com>
2016-01-02 17:12:35 +0100

    added ip print

--- src/mount_usb.py
+++ src/mount_usb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, glob, time
+import os, glob, time, socket
 
 def mount_usb(mounting_point):
     while True:
@@ -34,4 +34,5 @@ def mount_usb(mounting_point):
 
         sleep_seconds = 1
         print "Sleeping for %d seconds..." % sleep_seconds
+        print "IP: %s" % str([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
         time.sleep(sleep_seconds)
